2fb05bbe0fdb13b42503dbb6df595de9c5be3194
Ante Bozic <ante@Antes-MacBook-Pro.local>
2019-05-30 15:58:22 +0200

    updated Route in App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Treatment from './Steps/Treatment';
 import TimeTable from './Steps/TimeTable';
@@ -9,9 +8,9 @@ function App() {
 	return (
 		<div className="App">
 			<Switch>
-				<Route exact path="/" render={(props) => <Treatment {...props} />} />
-				<Route path="/time" render={(props) => <TimeTable {...props} />} />
-				<Route path="/userInfo" render={(props) => <UserInfo {...props} />} />
+				<Route exact path="/" component={Treatment} />
+				<Route path="/time" component={TimeTable} />
+				<Route path="/userInfo" component={UserInfo} />
 			</Switch>
 		</div>
 	);
