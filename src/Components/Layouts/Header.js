bb29c03d924458101a3dcfb29b9d6e64d17e4943
Ante Bozic <ante@Antes-MacBook-Pro.local>
2019-05-31 00:30:46 +0200

    obj destructuring from props

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { AppBar, Typography } from '@material-ui/core';
 
-export default (props) => {
-	const { titleText } = props;
-	return (
-		<AppBar position="static" color="primary" style={{ padding: 15 }}>
-			<Typography variant="h3" align="center" color="inherit">
-				{titleText}
-			</Typography>
-		</AppBar>
-	);
-};
+export default ({ titleText }) => (
+	<AppBar position="static" color="primary" style={{ padding: 15 }}>
+		<Typography variant="h3" align="center" color="inherit">
+			{titleText}
+		</Typography>
+	</AppBar>
+);
